1e70787b203e7afddfb802a22b3752bac30c5116
zdtgit <kconline77@gmail.com>
2024-01-28 23:16:54 +0900

    feat: Remove MiniMessage

--- src/main/kotlin/com/activation/velocity/extensions/string/StringExtension.kt
+++ src/main/kotlin/com/activation/velocity/extensions/string/StringExtension.kt
@@ -1,16 +1,7 @@
 package com.activation.velocity.extensions.string
 
 import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.minimessage.MiniMessage
-import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
 
 val String.legacy: Component
     get() = LegacyComponentSerializer.legacyAmpersand().deserialize(this)
-
-val String.mini: Component
-    get() = MiniMessage.miniMessage().deserialize(this)
-
-fun String.mini(tagResolver: TagResolver): Component {
-    return MiniMessage.miniMessage().deserialize(this, tagResolver)
-}
