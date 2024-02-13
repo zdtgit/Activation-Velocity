cdd53f30fceec514dfeae9a33b173b2c90d6c4d3
zdtgit <kconline77@gmail.com>
2024-02-13 21:09:10 +0900

    feat: Add MiniMessage

--- src/main/kotlin/com/activation/velocity/extensions/string/StringExtension.kt
+++ src/main/kotlin/com/activation/velocity/extensions/string/StringExtension.kt
@@ -1,7 +1,12 @@
 package com.activation.velocity.extensions.string
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.TextDecoration
+import net.kyori.adventure.text.minimessage.MiniMessage
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
 
 val String.legacy: Component
     get() = LegacyComponentSerializer.legacyAmpersand().deserialize(this)
+
+val String.mini: Component
+    get() = MiniMessage.miniMessage().deserialize(this).decoration(TextDecoration.ITALIC, false)
