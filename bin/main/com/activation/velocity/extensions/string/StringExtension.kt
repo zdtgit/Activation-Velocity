ff352dffa6f8d2d412f3da87cd185a2d0d6bc649
zdt <104686760+zdtgit@users.noreply.github.com>
2024-02-13 21:10:44 +0900

    Delete bin/main/com/activation/velocity directory

--- bin/main/com/activation/velocity/extensions/string/StringExtension.kt
+++
@@ -1,7 +0,0 @@
-package com.activation.velocity.extensions.string
-
-import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
-
-val String.legacy: Component
-    get() = LegacyComponentSerializer.legacyAmpersand().deserialize(this)
